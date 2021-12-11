83396dcb6aa3dbc6f8ba0e62233dab61a9c60338
piyo-toyou <61530828+piyo-toyou@users.noreply.github.com>
2021-12-11 20:03:37 +0900

    stage6

--- D_Infinity_32.py
+++ D_Infinity_32.py
@@ -21,7 +21,7 @@ returnarrayF = np.zeros(myarray.shape)
 
 def Around(arr, i, j):
     h_above = np.array((arr[i-1][j-1:j+2]))
-    h_mid = np.array((arr[i][j-1], np.nan, arr[i][j+1]))
+    h_mid = np.array((arr[i][j-1], 9999, arr[i][j+1]))
     h_below = np.array((arr[i+1][j-1:j+2]))
     return np.vstack((h_above, h_mid, h_below))
 
@@ -45,35 +45,36 @@ def Flag(arr, i, j):
 
 def DInfinity(ij):
     i, j = ij
+    arr = myarray
     try:
         b = [[0,1],[1,-1],[1,1],[2,-1],[2,1],[3,-1],[3,1],[4,-1]]
         
-        s1en = (myarray[i,j]-myarray[i,j+1])/dx
-        s2en = (myarray[i,j+1]-myarray[i-1,j+1])/dy
-        s0en = (myarray[i,j]-myarray[i-1,j+1])/dxy
-        s1ne = (myarray[i,j]-myarray[i-1,j])/dx 
-        s2ne = (myarray[i-1,j]-myarray[i-1,j+1])/dy
-        s1nw = (myarray[i,j]-myarray[i-1,j])/dx
-        s2nw = (myarray[i-1,j]-myarray[i-1,j-1])/dy
-        s0nw = (myarray[i,j]-myarray[i-1,j-1])/dxy
-        s1wn = (myarray[i,j]-myarray[i,j-1])/dx
-        s2wn = (myarray[i,j-1]-myarray[i-1,j-1])/dy
-        s1ws = (myarray[i,j]-myarray[i,j-1])/dx
-        s2ws = (myarray[i,j-1]-myarray[i+1,j-1])/dy
-        s0ws = (myarray[i,j]-myarray[i+1,j-1])/dxy
-        s1sw = (myarray[i,j]-myarray[i+1,j])/dx
-        s2sw = (myarray[i+1,j]-myarray[i+1,j-1])/dy
-        s1se = (myarray[i,j]-myarray[i+1,j])/dx
-        s2se = (myarray[i+1,j]-myarray[i+1,j+1])/dy
-        s0se = (myarray[i,j]-myarray[i+1,j+1])/dxy
-        s1es = (myarray[i,j]-myarray[i,j+1])/dx
-        s2es = (myarray[i,j+1]-myarray[i+1,j+1])/dy
+        s1en = (arr[i,j]-arr[i,j+1])/dx
+        s2en = (arr[i,j+1]-arr[i-1,j+1])/dy
+        s0en = (arr[i,j]-arr[i-1,j+1])/dxy
+        s1ne = (arr[i,j]-arr[i-1,j])/dx 
+        s2ne = (arr[i-1,j]-arr[i-1,j+1])/dy
+        s1nw = (arr[i,j]-arr[i-1,j])/dx
+        s2nw = (arr[i-1,j]-arr[i-1,j-1])/dy
+        s0nw = (arr[i,j]-arr[i-1,j-1])/dxy
+        s1wn = (arr[i,j]-arr[i,j-1])/dx
+        s2wn = (arr[i,j-1]-arr[i-1,j-1])/dy
+        s1ws = (arr[i,j]-arr[i,j-1])/dx
+        s2ws = (arr[i,j-1]-arr[i+1,j-1])/dy
+        s0ws = (arr[i,j]-arr[i+1,j-1])/dxy
+        s1sw = (arr[i,j]-arr[i+1,j])/dx
+        s2sw = (arr[i+1,j]-arr[i+1,j-1])/dy
+        s1se = (arr[i,j]-arr[i+1,j])/dx
+        s2se = (arr[i+1,j]-arr[i+1,j+1])/dy
+        s0se = (arr[i,j]-arr[i+1,j+1])/dxy
+        s1es = (arr[i,j]-arr[i,j+1])/dx
+        s2es = (arr[i,j+1]-arr[i+1,j+1])/dy
 
         #1 east -> north
         if s1en > 0:
             ren_temp = atan(s2en/s1en)
             if ren_temp > dtan:
-                ren = dtan; sen = (myarray[i,j]-myarray[i-1,j+1])/dxy
+                ren = dtan; sen = (arr[i,j]-arr[i-1,j+1])/dxy
             elif ren_temp > 0:
                 ren = ren_temp; sen = sqrt(pow(s1en,2)+pow(s2en,2))
             else:
@@ -88,7 +89,7 @@ def DInfinity(ij):
         if s1ne > 0:
             rne_temp = atan(s2ne/s1ne)
             if rne_temp > dtan:
-                rne = dtan; sne = (myarray[i,j]-myarray[i-1,j+1])/dxy
+                rne = dtan; sne = (arr[i,j]-arr[i-1,j+1])/dxy
             elif rne_temp > 0:
                 rne = rne_temp; sne = sqrt(pow(s1ne,2)+pow(s2ne,2))
             else:
@@ -103,7 +104,7 @@ def DInfinity(ij):
         if s1nw > 0:
             rnw_temp = atan(s2nw/s1nw)
             if rnw_temp > dtan:
-                rnw = dtan; snw = (myarray[i,j]-myarray[i-1,j-1])/dxy
+                rnw = dtan; snw = (arr[i,j]-arr[i-1,j-1])/dxy
             elif rnw_temp > 0:
                 rnw = rnw_temp; snw = sqrt(pow(s1nw,2)+pow(s2nw,2))
             else:
@@ -118,7 +119,7 @@ def DInfinity(ij):
         if s1wn > 0:
             rwn_temp = atan(s2wn/s1wn)
             if rwn_temp > dtan:
-                rwn = dtan; swn = (myarray[i,j]-myarray[i-1,j-1])/dxy
+                rwn = dtan; swn = (arr[i,j]-arr[i-1,j-1])/dxy
             elif rwn_temp > 0:
                 rwn = rwn_temp; swn = sqrt(pow(s1wn,2)+pow(s2wn,2))
             else:
@@ -133,7 +134,7 @@ def DInfinity(ij):
         if s1ws > 0:
             rws_temp = atan(s2ws/s1ws)
             if rws_temp > dtan:
-                rws = dtan; sws = (myarray[i,j]-myarray[i+1,j-1])/dxy
+                rws = dtan; sws = (arr[i,j]-arr[i+1,j-1])/dxy
             elif rws_temp > 0:
                 rws = rws_temp; sws = sqrt(pow(s1ws,2)+pow(s2ws,2))
             else:
@@ -148,7 +149,7 @@ def DInfinity(ij):
         if s1sw > 0:
             rsw_temp = atan(s2sw/s1sw)
             if rsw_temp > dtan:
-                rsw = dtan; ssw = (myarray[i,j]-myarray[i+1,j-1])/dxy
+                rsw = dtan; ssw = (arr[i,j]-arr[i+1,j-1])/dxy
             elif rsw_temp > 0:
                 rsw = rsw_temp; ssw = sqrt(pow(s1sw,2)+pow(s2sw,2))
             else:
@@ -163,7 +164,7 @@ def DInfinity(ij):
         if s1se > 0:
             rse_temp = atan(s2se/s1se)
             if rse_temp > dtan:
-                rse = dtan; sse = (myarray[i,j]-myarray[i+1,j+1])/dxy
+                rse = dtan; sse = (arr[i,j]-arr[i+1,j+1])/dxy
             elif rse_temp > 0:
                 rse = rse_temp; sse = sqrt(pow(s1se,2)+pow(s2se,2))
             else:
@@ -178,7 +179,7 @@ def DInfinity(ij):
         if s1es > 0:
             res_temp = atan(s2es/s1es)
             if res_temp > dtan:
-                res = dtan; ses = (myarray[i,j]-myarray[i+1,j+1])/dxy
+                res = dtan; ses = (arr[i,j]-arr[i+1,j+1])/dxy
             elif res_temp > 0:
                 res = res_temp; ses = sqrt(pow(s1es,2)+pow(s2es,2))
             else:
@@ -207,23 +208,42 @@ def DInfinity(ij):
 
 def Dinfinity_Receive(args):
     i, j, s, d = args
-    returnarrayS[i][j] = s
-    returnarrayD[i][j] = d
+    returnarrayS[i][j], returnarrayD[i][j] = s, d
+
+def D8(t):
+    if t[0] == -1:
+        d8 = 90 + t[1] * -45
+    elif t[0] == 0:
+        d8 = 90 + t[1] * -90
+    else:
+        d8 = 270 + t[1] * 45
+    return d8
 
 def Sink(dem, flag, dinf, i, j):
     dem_copy = copy(dem)
     target_area = np.zeros((Ysize, Xsize), dtype=int)
     target_area[i][j] = 1
-    my_around = Around(flag, i, j)
-    if any((1 in my_around)):
-        ind_temp = np.argwhere(my_around == 1)
-        pass
-    else:
-        dinf[i][j] = 300
+    my_around = Around(dem, i, j)
+
+    # 複数点を捉えるために、np.whereを使用する方が厳密
+    # 対象領域周囲から、最小標高点を探す
+    min_value = np.min(my_around)
+    min_idx = np.unravel_index(np.argmin(my_around), my_around.shape)
+    GMI = np.array((i, j)) + min_idx - np.array((1, 1)) # global min index
+    target_area[GMI[0], GMI[1]] = 1
+    flag[GMI[0], GMI[1]] = 1
+    dem_copy[i, j] = min_value # 窪地埋め処理
+
+    my_around = np.stack((my_around, Around(dem_copy, GMI[0], GMI[1])))
+    if (my_around < min_value).any(): # 周囲の点から流出点を探す
+        out_idx = np.unravel_index(np.argmin(my_around), my_around.shape)
+        GOI = GMI + out_idx[1:3] - np.array((1, 1)) # global out index
+        flag[GOI[0], GOI[1]] = 1.5
+        dinf[i][j] = D8(min_idx - np.array((1, 1)))
+        dinf[GMI[0], GMI[1]] = D8(out_idx[1:3] - np.array((1, 1)))
 
 def Flat(arr, i, j):
     pass
-
 
 #calculation
 #Flag
@@ -232,11 +252,11 @@ for i, j in np.ndindex(myarray.shape):
 
 #Regular cells
 args = map(DInfinity, np.argwhere(returnarrayF == 0))
-map(Dinfinity_Receive, args)
+for x in args:  Dinfinity_Receive(x)
 
 #Sink cells
-#for i, j in np.argwhere(returnarrayF == 2):
-#    Sink(myarray, returnarrayF, returnarrayD, i, j)
+for i, j in np.argwhere(returnarrayF == 2):
+    Sink(myarray, returnarrayF, returnarrayD, i, j)
 
 #Visualization
 cmap = cm.cool
