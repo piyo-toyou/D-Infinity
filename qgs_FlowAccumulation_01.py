0c346adfe33a07173ecb789f9f3404ed9832754e
piyo-toyou <61530828+piyo-toyou@users.noreply.github.com>
2022-01-17 17:27:28 +0900

    stage8.9

---
+++ qgs_FlowAccumulation_01.py
@@ -0,0 +1,231 @@
+#Import Modules
+import numpy as np
+from math import sqrt, degrees, atan, pi
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessingParameterRasterLayer,
+                       QgsProcessingParameterNumber,
+                       QgsProcessingParameterBand,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFileDestination)
+from osgeo import gdal
+
+class FlowAccumulation(QgsProcessingAlgorithm):
+    
+    INPUT = "INPUT"
+    BAND = "BAND"
+    OUTPUT_FA = "OUTPUT_FA"
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return FlowAccumulation()
+
+    def name(self):
+        return 'flowaccumulation01'
+
+    def displayName(self):
+        return self.tr('Flow Accumulation 01')
+
+    def group(self):
+        return self.tr('KOGA Raster')
+
+    def groupId(self):
+        return 'kogaraster'
+
+    def shortHelpString(self):
+        return self.tr("No description")
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterRasterLayer(self.INPUT,self.tr('Input layer')))
+        self.addParameter(QgsProcessingParameterBand(self.BAND,self.tr('Bamd Number'),1,self.INPUT))
+        self.addParameter(QgsProcessingParameterFileDestination(self.OUTPUT_FA,self.tr('Output layer Accumulation'),self.tr('Tiff files (*.tif')))
+    
+    def processAlgorithm(self, parameters, context, feedback):
+        layer = self.parameterAsRasterLayer(parameters, self.INPUT, context)
+        band = self.parameterAsInt(parameters, self.BAND, context)
+        output_FA = self.parameterAsFileOutput(parameters, self.OUTPUT_FA, context)
+
+        filename = str(layer.source())
+        datasete = gdal.Open(filename, gdal.GA_ReadOnly)
+        Xsize = datasete.RasterXSize
+        Ysize = datasete.RasterYSize
+        GT = datasete.GetGeoTransform()
+        dx, dy = abs(GT[1]), abs(GT[5])
+        CSR = datasete.GetProjection()
+        my_band = datasete.GetRasterBand(band)
+
+        myarrayFD = my_band.ReadAsArray()
+        returnarrayFA = np.full(myarrayFD.shape, -0.5)
+
+        def Zero(arr, i, j):
+            if 135 < arr[i,j+1] < 225 or 180 < arr[i-1,j+1] < 270 or 225 < arr[i-1,j] < 315 \
+                    or 270 < arr[i-1,j-1] or 315 < arr[i,j-1] or 0 <= arr[i,j-1] < 45 or 0 < arr[i+1,j-1] < 90 \
+                    or 45 < arr[i+1,j] < 135 or 90 < arr[i+1,j+1] < 180: 
+                return -1.0
+            else:
+                return 0
+
+        def ACM(arr1, arr2, i, j):
+            d = 0
+            # east
+            if arr1[i,j+1] <= 135 or arr1[i,j+1] >= 225:
+                pass
+            elif 135 < arr1[i,j+1] < 180 and arr2[i,j+1] > 0:
+                d += (arr2[i,j+1]+1)*((arr1[i,j+1]-135)/45)
+            elif 135 < arr1[i,j+1] < 180 and arr2[i,j+1] == 0:
+                d += (arr1[i,j+1]-135)/45
+            elif 180 <= arr1[i,j+1] < 225 and arr2[i,j+1] > 0:
+                d += (arr2[i,j+1]+1)*((225-arr1[i,j+1])/45)
+            elif 180 <= arr1[i,j+1] < 225 and arr2[i,j+1] == 0:
+                d += (225-arr1[i,j+1])/45
+            elif arr1[i,j+1] == -1:
+                pass
+            else:
+                return False
+            
+            #north - east
+            if arr1[i-1,j+1] <= 180 or arr1[i-1,j+1] >= 270:
+                pass
+            elif 180 < arr1[i-1,j+1] < 225 and arr2[i-1,j+1] > 0:
+                d += (arr2[i-1,j+1]+1)*((arr1[i-1,j+1]-180)/45)
+            elif 180 < arr1[i-1,j+1] < 225 and arr2[i-1,j+1] == 0:
+                d += (arr1[i-1,j+1]-180)/45
+            elif 225 <= arr1[i-1,j+1] < 270 and arr2[i-1,j+1] > 0:
+                d += (arr2[i-1,j+1]+1)*((270-arr1[i-1,j+1])/45)
+            elif 225 <= arr1[i-1,j+1] < 270 and arr2[i-1,j+1] == 0:
+                d += (270-arr1[i-1,j+1])/45
+            elif arr1[i-1,j+1] == -1:
+                pass
+            else:
+                return False
+            
+            # north
+            if arr1[i-1,j] <= 225 or arr1[i-1,j] >= 315:
+                pass
+            elif 225 < arr1[i-1,j] < 270 and arr2[i-1,j] > 0:
+                d += (arr2[i-1,j]+1)*((arr1[i-1,j]-225)/45)
+            elif 225 < arr1[i-1,j] < 270 and arr2[i-1,j] == 0:
+                d += (arr1[i-1,j]-225)/45
+            elif 270 <= arr1[i-1,j] < 315 and arr2[i-1,j] > 0:
+                d += (arr2[i-1,j]+1)*((315-arr1[i-1,j])/45)
+            elif 270 <= arr1[i-1,j] < 315 and arr2[i-1,j] == 0:
+                d += (315-arr1[i-1,j])/45
+            elif arr1[i-1,j] == -1:
+                pass
+            else:
+                return False
+
+            # north - west
+            if arr1[i-1,j-1] <= 270:
+                pass
+            elif 270 < arr1[i-1,j-1] < 315 and arr2[i-1,j-1] > 0:
+                d += (arr2[i-1,j-1]+1)*((arr1[i-1,j-1]-270)/45)
+            elif 270 < arr1[i-1,j-1] < 315 and arr2[i-1,j-1] == 0:
+                d += (arr1[i-1,j-1]-270)/45
+            elif 315 <= arr1[i-1,j-1] and arr2[i-1,j-1] > 0:
+                d += (arr2[i-1,j-1]+1)*((360-arr1[i-1,j-1])/45)
+            elif 315 <= arr1[i-1,j-1] and arr2[i-1,j-1] == 0:
+                d += (360-arr1[i-1,j-1])/45
+            elif arr1[i-1,j-1] == -1:
+                pass
+            else:
+                return False
+
+            # west
+            if arr1[i,j-1] >= 45 and arr1[i,j-1] <= 315:
+                pass
+            elif  0 <= arr1[i,j-1] < 45 and arr2[i,j-1] > 0:
+                d += (arr2[i,j-1]+1)*(1-(arr1[i,j-1]/45))
+            elif  0 <= arr1[i,j-1] < 45 and arr2[i,j-1] == 0:
+                d += 1-(arr1[i,j-1]/45)
+            elif 315 < arr1[i,j-1] and arr2[i,j-1] > 0:
+                d += (arr2[i,j-1]+1)*((arr1[i,j-1]-315)/45)
+            elif 315 < arr1[i,j-1] and arr2[i,j-1] == 0:
+                d += (arr1[i,j-1]-315)/45
+            elif arr1[i,j-1] == -1:
+                pass
+            else:
+                return False
+
+            # south - west
+            if arr1[i+1,j-1] >= 90 or arr1[i+1,j-1] <= 0:
+                pass
+            elif arr1[i+1,j-1] < 45 and arr2[i+1,j-1] > 0:
+                d += (arr2[i+1,j-1]+1)*(arr1[i+1,j-1]/45)
+            elif arr1[i+1,j-1] < 45 and arr2[i+1,j-1] == 0:
+                d += arr1[i+1,j-1]/45
+            elif 45 <= arr1[i+1,j-1] < 90 and arr2[i+1,j-1] > 0:
+                d += (arr2[i+1,j-1]+1)*((90-arr1[i+1,j-1])/45)
+            elif 45 <= arr1[i+1,j-1] < 90 and arr2[i+1,j-1] == 0:
+                d += (90-arr1[i+1,j-1])/45
+            elif arr1[i+1,j-1] == -1:
+                pass
+            else:
+                return False
+            
+            # south
+            if arr1[i+1,j] <= 45 or arr1[i+1,j] >= 135:
+                pass
+            elif 45 < arr1[i+1,j] < 90 and arr2[i+1,j] > 0:
+                d += (arr2[i+1,j]+1)*((arr1[i+1,j]-45)/45)
+            elif 45 < arr1[i+1,j] < 90 and arr2[i+1,j] == 0:
+                d += (arr1[i+1,j]-45)/45
+            elif 90 <= arr1[i+1,j] < 135 and arr2[i+1,j] > 0:
+                d += (arr2[i+1,j]+1)*((135-arr1[i+1,j])/45)
+            elif 90 <= arr1[i+1,j] < 135 and arr2[i+1,j] == 0:
+                d += (135-arr1[i+1,j])/45
+            elif arr1[i+1,j] == -1:
+                pass
+            else:
+                return False
+
+            # south - east
+            if arr1[i+1,j+1] <= 90 or arr1[i+1,j+1] >= 180:
+                pass
+            elif 90 < arr1[i+1,j+1] < 135 and arr2[i+1,j+1] > 0:
+                d += (arr2[i+1,j+1]+1)*((arr1[i+1,j+1]-90)/45)
+            elif 90 < arr1[i+1,j+1] < 135 and arr2[i+1,j+1] == 0:
+                d += (arr1[i+1,j+1]-90)/45
+            elif 135 <= arr1[i+1,j+1] < 180 and arr2[i+1,j+1] > 0:
+                d += (arr2[i+1,j+1]+1)*((180-arr1[i+1,j+1])/45)
+            elif 135 <= arr1[i+1,j+1] < 180 and arr2[i+1,j+1] == 0:
+                d += (180-arr1[i+1,j+1])/45
+            elif arr1[i+1,j+1] == -1:
+                pass
+            else:
+                return False
+
+            return d
+
+        #Calculation
+        #Zero        
+        for i, j in np.argwhere(myarrayFD != -1):
+            returnarrayFA[i][j] = Zero(myarrayFD, i, j)
+
+        #Flow Accumulation
+        loop = 0
+        while np.argwhere(returnarrayFA == -1).size:
+            for i, j in np.argwhere(returnarrayFA == -1):
+                f = ACM(myarrayFD, returnarrayFA, i, j)
+                if f:
+                    returnarrayFA[i][j] = f
+                else:
+                    pass
+            loop += 1
+            if loop == 10000:
+                break
+
+        dtype = gdal.GDT_Float32 #others: gdal.GDT_Byte, ...
+        out_band = 1 # バンド数
+        out_tiff_FA = gdal.GetDriverByName('GTiff').Create(output_FA, Xsize, Ysize, out_band, dtype)
+
+        out_tiff_FA.SetGeoTransform(GT) # 座標系指定
+        out_tiff_FA.SetProjection(CSR) # 空間情報を結合
+
+        out_tiff_FA.GetRasterBand(1).WriteArray(returnarrayFA)
+        out_tiff_FA.FlushCache()
+
+        return {self.OUTPUT_FA: output_FA}
+
---
+++ qgs_FlowDirection_11.py
@@ -0,0 +1,424 @@
+#Import Modules
+import numpy as np
+from math import sqrt, degrees, atan, pi
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessingParameterRasterLayer,
+                       QgsProcessingParameterNumber,
+                       QgsProcessingParameterBand,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFileDestination)
+from osgeo import gdal
+
+class FlowDirection(QgsProcessingAlgorithm):
+    
+    INPUT = "INPUT"
+    BAND = "BAND"
+    OUTPUT_S = "OUTPUT_S"
+    OUTPUT_D = "OUTPUT_D"
+
+    def tr(self, string):
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return FlowDirection()
+
+    def name(self):
+        return 'flowdirection11'
+
+    def displayName(self):
+        return self.tr('Flow Direction 11')
+
+    def group(self):
+        return self.tr('KOGA Raster')
+
+    def groupId(self):
+        return 'kogaraster'
+
+    def shortHelpString(self):
+        return self.tr("No description")
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterRasterLayer(self.INPUT,self.tr('Input layer')))
+        self.addParameter(QgsProcessingParameterBand(self.BAND,self.tr('Bamd Number'),1,self.INPUT))
+        self.addParameter(QgsProcessingParameterFileDestination(self.OUTPUT_S,self.tr('Output layer Slope'),self.tr('Tiff files (*.tif')))
+        self.addParameter(QgsProcessingParameterFileDestination(self.OUTPUT_D,self.tr('Output layer Direction'),self.tr('Tiff files (*.tif')))
+    
+    def processAlgorithm(self, parameters, context, feedback):
+        layer = self.parameterAsRasterLayer(parameters, self.INPUT, context)
+        band = self.parameterAsInt(parameters, self.BAND, context)
+        output_S = self.parameterAsFileOutput(parameters, self.OUTPUT_S, context)
+        output_D = self.parameterAsFileOutput(parameters, self.OUTPUT_D, context)
+
+        filename = str(layer.source())
+        datasete = gdal.Open(filename, gdal.GA_ReadOnly)
+        Xsize = datasete.RasterXSize
+        Ysize = datasete.RasterYSize
+        GT = datasete.GetGeoTransform()
+        dx, dy = abs(GT[1]), abs(GT[5])
+        dxy = sqrt(pow(dx,2)+pow(dy,2))
+        dtan = atan(dx/dy)
+        CSR = datasete.GetProjection()
+        my_band = datasete.GetRasterBand(band)
+
+        my_array = my_band.ReadAsArray()
+        returnarrayS = np.zeros(my_array.shape)
+        returnarrayD = np.full(my_array.shape, -1.0)
+        returnarrayF = np.zeros(my_array.shape)
+        def Around(arr, X):
+            if X.size == 2:
+                i, j = X
+                h_above = arr[i-1][j-1:j+2]
+                h_mid = np.array((arr[i][j-1], 9999, arr[i][j+1]))
+                h_below = arr[i+1][j-1:j+2]
+                return np.vstack((h_above, h_mid, h_below))
+            else:
+                temp_around = np.arange(3)
+                for x1, x2 in enumerate(X):
+                    X_d1 = X - X[x1] # 対象範囲との被りを検出
+                    X_d2 = X_d1[np.all(-1<=X_d1, axis=1)]
+                    X_d3 = X_d2[np.all(X_d2<=1, axis=1)]
+
+                    i, j = x2 # 周囲の標高値を抜き出し
+                    h_above = arr[i-1][j-1:j+2]
+                    h_mid = np.array((arr[i][j-1], 9999, arr[i][j+1]))
+                    h_below = arr[i+1][j-1:j+2]
+                    h_marge = np.vstack((h_above, h_mid, h_below))
+                    for s, t in X_d3:
+                        h_marge[s+1, t+1] = 9999
+                    temp_around = np.vstack((temp_around, h_marge))
+                return temp_around[1:]
+
+        def Flag(arr, i, j):
+            if i-1 >= 0 and j-1 >= 0 and i+1 < Ysize and j+1 < Xsize:
+                if (Around(arr, np.array((i, j))) < np.array((-999))).any():
+                    return np.nan
+                try:
+                    h0 = arr[i][j]
+                    h_around = Around(arr, np.array((i, j)))
+                    if (h0 > h_around).any():
+                        return 0 #Dinfinity
+                    elif (h0 == h_around).any():
+                        return 1 #Flat
+                    else:
+                        return 2 #Sink
+                except:
+                    return np.nan #Error
+            else:
+                return np.nan
+
+        def DInfinity(ij):
+            i, j = ij
+            arr = my_array
+            try:
+                b = [[0,1],[1,-1],[1,1],[2,-1],[2,1],[3,-1],[3,1],[4,-1]]
+                
+                s1en = (arr[i,j]-arr[i,j+1])/dx
+                s2en = (arr[i,j+1]-arr[i-1,j+1])/dy
+                s0en = (arr[i,j]-arr[i-1,j+1])/dxy
+                s1ne = (arr[i,j]-arr[i-1,j])/dx 
+                s2ne = (arr[i-1,j]-arr[i-1,j+1])/dy
+                s1nw = (arr[i,j]-arr[i-1,j])/dx
+                s2nw = (arr[i-1,j]-arr[i-1,j-1])/dy
+                s0nw = (arr[i,j]-arr[i-1,j-1])/dxy
+                s1wn = (arr[i,j]-arr[i,j-1])/dx
+                s2wn = (arr[i,j-1]-arr[i-1,j-1])/dy
+                s1ws = (arr[i,j]-arr[i,j-1])/dx
+                s2ws = (arr[i,j-1]-arr[i+1,j-1])/dy
+                s0ws = (arr[i,j]-arr[i+1,j-1])/dxy
+                s1sw = (arr[i,j]-arr[i+1,j])/dx
+                s2sw = (arr[i+1,j]-arr[i+1,j-1])/dy
+                s1se = (arr[i,j]-arr[i+1,j])/dx
+                s2se = (arr[i+1,j]-arr[i+1,j+1])/dy
+                s0se = (arr[i,j]-arr[i+1,j+1])/dxy
+                s1es = (arr[i,j]-arr[i,j+1])/dx
+                s2es = (arr[i,j+1]-arr[i+1,j+1])/dy
+
+                #1 east -> north
+                if s1en > 0:
+                    ren_temp = atan(s2en/s1en)
+                    if ren_temp > dtan:
+                        ren = dtan; sen = (arr[i,j]-arr[i-1,j+1])/dxy
+                    elif ren_temp > 0:
+                        ren = ren_temp; sen = sqrt(pow(s1en,2)+pow(s2en,2))
+                    else:
+                        ren = 0.00; sen = s1en
+                else:
+                    if  s0en > 0:
+                        ren = dtan; sen = s0en
+                    else:
+                        ren = 0.00; sen = s1en
+
+                #2 north -> east
+                if s1ne > 0:
+                    rne_temp = atan(s2ne/s1ne)
+                    if rne_temp > dtan:
+                        rne = dtan; sne = (arr[i,j]-arr[i-1,j+1])/dxy
+                    elif rne_temp > 0:
+                        rne = rne_temp; sne = sqrt(pow(s1ne,2)+pow(s2ne,2))
+                    else:
+                        rne = 0.00; sne = s1ne
+                else:
+                    if s0en > 0:
+                        rne = dtan; sne = s0en
+                    else:
+                        rne = 0.00; sne = s1ne
+
+                #3 north -> west
+                if s1nw > 0:
+                    rnw_temp = atan(s2nw/s1nw)
+                    if rnw_temp > dtan:
+                        rnw = dtan; snw = (arr[i,j]-arr[i-1,j-1])/dxy
+                    elif rnw_temp > 0:
+                        rnw = rnw_temp; snw = sqrt(pow(s1nw,2)+pow(s2nw,2))
+                    else:
+                        rnw = 0.00; snw = s1nw
+                else:
+                    if  s0nw > 0:
+                        rnw = dtan; snw = s0nw
+                    else:
+                        rnw = 0.00; snw = s1nw
+
+                #4 west -> north
+                if s1wn > 0:
+                    rwn_temp = atan(s2wn/s1wn)
+                    if rwn_temp > dtan:
+                        rwn = dtan; swn = (arr[i,j]-arr[i-1,j-1])/dxy
+                    elif rwn_temp > 0:
+                        rwn = rwn_temp; swn = sqrt(pow(s1wn,2)+pow(s2wn,2))
+                    else:
+                        rwn = 0.00; swn = s1wn
+                else:
+                    if s0nw > 0:
+                        rwn = dtan; swn = s0nw
+                    else:
+                        rwn = 0.00; swn = s1wn
+
+                #5 west -> south
+                if s1ws > 0:
+                    rws_temp = atan(s2ws/s1ws)
+                    if rws_temp > dtan:
+                        rws = dtan; sws = (arr[i,j]-arr[i+1,j-1])/dxy
+                    elif rws_temp > 0:
+                        rws = rws_temp; sws = sqrt(pow(s1ws,2)+pow(s2ws,2))
+                    else:
+                        rws = 0.00; sws = s1ws
+                else:
+                    if s0ws > 0:
+                        rws = dtan; sws = s0ws
+                    else:
+                        rws = 0.00; sws = s1ws
+
+                #6 south -> west
+                if s1sw > 0:
+                    rsw_temp = atan(s2sw/s1sw)
+                    if rsw_temp > dtan:
+                        rsw = dtan; ssw = (arr[i,j]-arr[i+1,j-1])/dxy
+                    elif rsw_temp > 0:
+                        rsw = rsw_temp; ssw = sqrt(pow(s1sw,2)+pow(s2sw,2))
+                    else:
+                        rsw = 0.00; ssw = s1sw
+                else:
+                    if  s0ws > 0:
+                        rsw = dtan; ssw = s0ws
+                    else:
+                        rsw = 0.00; ssw = s1sw
+
+                #7 south -> east
+                if s1se > 0:
+                    rse_temp = atan(s2se/s1se)
+                    if rse_temp > dtan:
+                        rse = dtan; sse = (arr[i,j]-arr[i+1,j+1])/dxy
+                    elif rse_temp > 0:
+                        rse = rse_temp; sse = sqrt(pow(s1se,2)+pow(s2se,2))
+                    else:
+                        rse = 0.00; sse = s1se
+                else:
+                    if s0se > 0:
+                        rse = dtan; sse = s0se
+                    else:
+                        rse = 0.00; sse = s1se
+
+                #8 east -> south
+                if s1es > 0:
+                    res_temp = atan(s2es/s1es)
+                    if res_temp > dtan:
+                        res = dtan; ses = (arr[i,j]-arr[i+1,j+1])/dxy
+                    elif res_temp > 0:
+                        res = res_temp; ses = sqrt(pow(s1es,2)+pow(s2es,2))
+                    else:
+                        res = 0.00; ses = s1es
+                else:
+                    if s0se > 0:
+                        res = dtan; ses = s0se
+                    else:
+                        res = 0.00; ses = s1es
+                
+                r_tuple = (ren, rne, rnw, rwn, rws, rsw, rse, res)
+                s_tuple = (sen, sne, snw, swn, sws, ssw, sse, ses)
+                
+                smax = max(s_tuple)
+                if smax > 0:
+                    sdeg = degrees(atan(smax))
+                    sid = s_tuple.index(smax)
+                    r = r_tuple[sid]
+                    rg = degrees(b[sid][1]*r+b[sid][0]*pi/2)
+                else:
+                    sdeg = 0.00
+                    rg = -1
+                return i, j, sdeg, rg
+            except:
+                return i, j, np.nan, np.nan
+
+        def Dinfinity_Receive(args):
+            i, j, s, d = args
+            returnarrayS[i][j], returnarrayD[i][j] = s, d
+
+        def D8(t_area, t_idx, t_point, out_point): # 対象範囲全体、番号、座標と、流出点座標
+            out_check1 = out_point - t_point
+            if -1 <= out_check1[0] <= 1 and -1 <= out_check1[1] <= 1:
+                t = np.squeeze(out_check1)
+            else:
+                T_d1 = np.delete(t_area, t_idx, 0) # target diffrence
+                T_d1 = T_d1 - t_point
+                T_d1 = T_d1[np.all(-1<=T_d1, axis=1)]
+                T_d1 = T_d1[np.all(T_d1<=1, axis=1)] # 隣接する範囲を探索
+                if T_d1.size == 2: # 1箇所と接する場合
+                    t = np.squeeze(T_d1)
+                else: # 複数個所と接する場合
+                    GT_d1 = T_d1 + t_point # global target diffrence
+                    GT_d2 = GT_d1 - out_point
+                    GT_d3 = np.array([np.linalg.norm(i) for i in GT_d2])
+                    t = T_d1[np.argmin(GT_d3)]
+            if t[0] == -1:
+                d8 = 90 + t[1] * -45
+            elif t[0] == 0:
+                d8 = 90 + t[1] * -90
+            else:
+                d8 = 270 + t[1] * 45
+            return d8
+
+        def Flat(dem, flag, dinf, i, j):
+            target_area = np.array((i, j))
+            while True:
+                flag_around = Around(flag, target_area)
+                if 1 in flag_around:
+                    flat_area = np.where(flag_around == 1)
+                    flat_area_idx = np.vstack((flat_area[0], flat_area[1])).T
+                    for s in range(len(flat_area_idx)):
+                        if target_area.size == 2:
+                            G = target_area
+                        else:
+                            G = target_area[flat_area_idx[s][0]//3]
+                        F = ((flat_area_idx[s][0]%3, flat_area_idx[s][1]))
+                        I = np.array((1, 1))
+                        GFI = G + F - I # global flat index
+                        target_area = np.vstack((target_area, GFI))
+                else:
+                    break
+            if target_area.size >= 4:
+                target_area = np.unique(target_area, axis=0)
+            my_around = Around(dem, target_area)
+            for p in range(100):
+                # 複数点を捉えるために、np.whereを使用する方が厳密
+                # 対象領域周囲から、最小標高点を探す
+                min_value = np.min(my_around)
+                min_idx = np.unravel_index(np.argmin(my_around), my_around.shape)
+                if target_area.size == 2:   GMI = target_area + min_idx - np.array((1, 1)) # global min index
+                else:       GMI = target_area[min_idx[0]//3] + ((min_idx[0]%3, min_idx[1])) - np.array((1, 1))
+                if 0 in GMI or Ysize-1 == GMI[0] or Xsize-1 == GMI[1]:
+                    break
+                target_area = np.vstack((target_area, GMI))
+                for u, v in target_area:
+                    dem[u, v] = min_value # 窪地埋め処理
+                my_around = Around(dem, target_area) # 対象領域の周囲を更新
+                if (my_around < min_value).any(): # 周囲の点から流出点を探す
+                    break
+            out_idx = np.unravel_index(np.argmin(my_around), my_around.shape)
+            GOI = target_area[out_idx[0]//3] + ((out_idx[0]%3, out_idx[1])) - np.array((1, 1)) # global out index
+            try:
+                for q1, q2 in enumerate(target_area):
+                    dinf[q2[0], q2[1]] = D8(target_area, q1, q2, GOI)
+                    flag[q2[0], q2[1]] = 0.75
+            except:
+                GOI = target_area + out_idx - np.array((1, 1))
+                target_area = target_area[np.newaxis, :]
+                for q1, q2 in enumerate(target_area):
+                    dinf[q2[0], q2[1]] = D8(target_area, q1, q2, GOI)
+                    flag[q2[0], q2[1]] = 0.75
+
+        def Sink(dem, flag, dinf, i, j):
+            target_area = np.array((i, j))
+            my_around = Around(dem, target_area)
+            for p in range(100):
+                # 複数点を捉えるために、np.whereを使用する方が厳密
+                # 対象領域周囲から、最小標高点を探す
+                min_value = np.min(my_around)
+                min_idx = np.unravel_index(np.argmin(my_around), my_around.shape)
+                if not p:   GMI = np.array((i, j)) + min_idx - np.array((1, 1)) # global min index
+                else:       GMI = target_area[min_idx[0]//3] + ((min_idx[0]%3, min_idx[1])) - np.array((1, 1))
+                if 0 in GMI or Ysize-1 == GMI[0] or Xsize-1 == GMI[1]:
+                    break
+                target_area = np.vstack((target_area, GMI))
+                for u, v in target_area:
+                    dem[u, v] = min_value # 窪地埋め処理
+                my_around = Around(dem, target_area) # 対象領域の周囲を更新
+                if (my_around < min_value).any(): # 周囲の点から流出点を探す
+                    break
+            out_idx = np.unravel_index(np.argmin(my_around), my_around.shape)
+            GOI = target_area[out_idx[0]//3] + ((out_idx[0]%3, out_idx[1])) - np.array((1, 1)) # global out index
+            try:
+                for q1, q2 in enumerate(target_area):
+                    dinf[q2[0], q2[1]] = D8(target_area, q1, q2, GOI)
+                    flag[q2[0], q2[1]] = 1.75
+            except:
+                GOI = target_area + out_idx - np.array((1, 1))
+                target_area = target_area[np.newaxis, :]
+                for q1, q2 in enumerate(target_area):
+                    dinf[q2[0], q2[1]] = D8(target_area, q1, q2, GOI)
+                    flag[q2[0], q2[1]] = 1.75
+
+        #calculation
+        #Flag
+        for i, j in np.ndindex(my_array.shape):
+            returnarrayF[i][j] = Flag(my_array, i, j)
+
+        #Regular cells
+        args = map(DInfinity, np.argwhere(returnarrayF == 0))
+        for x in args:  Dinfinity_Receive(x)
+
+        #Flat cells
+        while True:
+            if np.argwhere(returnarrayF == 1).size:
+                ij = np.argwhere(returnarrayF == 1)
+                i, j = ij[np.random.choice(ij.shape[0],1)][0]
+            else:
+                break
+            Flat(my_array, returnarrayF, returnarrayD, i, j)
+
+        #Sink cells
+        while True:
+            if np.argwhere(returnarrayF == 2).size:
+                ij = np.argwhere(returnarrayF == 2)
+                i, j = ij[np.random.choice(ij.shape[0],1)][0]
+            else:
+                break
+            Sink(my_array, returnarrayF, returnarrayD, i, j)
+
+        dtype = gdal.GDT_Float32 #others: gdal.GDT_Byte, ...
+        out_band = 1 # バンド数
+        out_tiff_S = gdal.GetDriverByName('GTiff').Create(output_S, Xsize, Ysize, out_band, dtype)
+        out_tiff_D = gdal.GetDriverByName('GTiff').Create(output_D, Xsize, Ysize, out_band, dtype) # 空の出力ファイル
+
+        out_tiff_S.SetGeoTransform(GT) # 座標系指定
+        out_tiff_S.SetProjection(CSR) # 空間情報を結合
+        out_tiff_D.SetGeoTransform(GT)
+        out_tiff_D.SetProjection(CSR)
+
+        out_tiff_S.GetRasterBand(1).WriteArray(returnarrayS)
+        out_tiff_S.FlushCache()
+        out_tiff_D.GetRasterBand(1).WriteArray(returnarrayD)
+        out_tiff_D.FlushCache()
+
+        return {self.OUTPUT_S: output_S, self.OUTPUT_D: output_D}
+
