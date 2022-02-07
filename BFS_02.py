ae602c899d8174748ae66306f7a655afd91f554b
piyo-toyou <61530828+piyo-toyou@users.noreply.github.com>
2022-02-07 22:46:46 +0900

    stage9.2.6

--- BFS_02.py
+++ BFS_02.py
@@ -17,9 +17,10 @@ mazeXedge = np.min((goi[1], np.min(target_area.T[1])))
 mazeY1 = np.max((goi[0], np.max(target_area.T[0])))- mazeYedge + 1
 mazeX1 = np.max((goi[1], np.max(target_area.T[1])))- mazeXedge + 1
 maze = np.full((mazeY1, mazeX1), 9, dtype=np.int8)
-route = np.zeros((mazeY1, mazeX1), dtype=np.int8)
+route = np.full((mazeY1, mazeX1), 9, dtype=np.int8)
 for xy in target_area-np.array((mazeYedge, mazeXedge)): # 対象範囲を白抜き
     maze[xy[0], xy[1]] = 0
+    route[xy[0], xy[1]] = 0
 maze[goi[0]-mazeYedge, goi[1]-mazeXedge] = 1 # 流出点を設定
 maze = np.vstack((np.full((mazeX1,), 9), maze, np.full((mazeX1,), 9))) # 周囲を埋め
 maze = np.hstack((np.full((mazeY1+2,1), 9), maze, np.full((mazeY1+2,1), 9)))
@@ -30,24 +31,33 @@ def Maze(start):
     pos = start
 
     while len(pos) > 0:#探索可能ならTrue
-        x, y, depth = pos.pop(0) #リストから探索する位置を取得
+        y, x, depth = pos.pop(0) #リストから探索する位置を取得
 
         #ゴールについた時点で終了
-        if maze[x][y] == 1:
-            return [(x, y), depth]
+        if maze[y][x] == 1:
+            return [(y, x), depth]
 
         #探索済みとしてセット
-        maze[x][y] = 2
+        maze[y][x] = 2
 
         #現在位置の上下左右を探索：〇<2は壁でもなく探索済みでもないものを示す
-        if maze[x-1][y] < 2:#左
-            pos.append([x-1, y, depth + 1])
-        if maze[x+1][y] < 2:#右
-            pos.append([x+1, y, depth + 1])
-        if maze[x][y-1] < 2:#上
-            pos.append([x, y-1, depth + 1])
-        if maze[x][y+1] < 2:#下
-            pos.append([x, y+1, depth + 1])
+        if maze[y-1][x] < 2:#上
+            pos.append([y-1, x, depth + 1])
+        if maze[y-1][x-1] < 2:#左上
+            pos.append([y-1, x-1, depth + 1])
+        if maze[y+1][x] < 2:#下
+            pos.append([y+1, x, depth + 1])
+        if maze[y+1][x-1] < 2:#左下
+            pos.append([y+1, x-1, depth + 1])
+        if maze[y][x-1] < 2:#左
+            pos.append([y, x-1, depth + 1])
+        if maze[y][x+1] < 2:#右
+            pos.append([y, x+1, depth + 1])
+        if maze[y-1][x+1] < 2:#右上
+            pos.append([y-1, x-1, depth + 1])
+        if maze[y+1][x+1] < 2:#右下
+            pos.append([y+1, x+1, depth + 1])
+        print(pos)
 
     return False
 
